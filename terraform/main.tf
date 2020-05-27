bdd5879ba7f883f8f845c1db0cd813e70c9d02ac
reddy1praveen <32567752+reddy1praveen@users.noreply.github.com>
2020-05-27 09:16:12 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "Lian-s3-bucket"
+  bucket = "lian-s3-bucket"
   acl    = "private"
   region = "eu-west-2"
 
